a30df454e89b4b13dd2c9fe412285f965bc6cde8
luis liguori <lugaliguori@gmail.com>
2019-04-29 23:06:52 -0400

    added form line

--- resources/views/layouts/admin/doctors-add.blade.php
+++ resources/views/layouts/admin/doctors-add.blade.php
@@ -15,12 +15,13 @@
                                     <div class="col-sm-10"><input class="form-control" name="nombre" id="nombre" required></div>
 
                                 </div>
-                                                            <div class="hr-line-dashed"></div>
+                                <div class="hr-line-dashed"></div>
                                 <div class="form-group  row"><label class="col-sm-2 col-form-label">Apellido</label>
 
                                     <div class="col-sm-10"><input class="form-control" name="apellido" id="apellido" required></div>
 
                                 </div>
+                                <div class="hr-line-dashed"></div>
                                 <div class="form-group  row"><label class="col-sm-2 col-form-label">Cédula</label>
 
                                     <div class="col-sm-10"><input type="text" name="cedula" id="cedula" class="form-control" required></div>
