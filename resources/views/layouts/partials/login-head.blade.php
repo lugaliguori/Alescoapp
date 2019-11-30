57508099b378bc87b1cc96abf3916c3858a7663d
luis liguori <lugaliguori@gmail.com>
2019-11-30 00:39:03 -0400

    adding comments for local assets

--- resources/views/layouts/partials/login-head.blade.php
+++ resources/views/layouts/partials/login-head.blade.php
@@ -3,11 +3,28 @@
 
     <title>Alesco</title>
 
+    <!--
     <link href="{{  secure_asset('css/bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{  secure_asset('font-awesome/css/font-awesome.css')}}" rel="stylesheet">
 
     <link href="{{  secure_asset('css/animate.css')}}" rel="stylesheet">
     <link href="{{  secure_asset('css/style.css')}}" rel="stylesheet">
+ 
+
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="{{  secure_asset('/js/patient-add.js') }}"></script>
+-->
+
+    <link href="{{  asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{  asset('font-awesome/css/font-awesome.css')}}" rel="stylesheet">
+
+    <link href="{{  asset('css/animate.css')}}" rel="stylesheet">
+    <link href="{{ 	asset('css/style.css')}}" rel="stylesheet">
  
     <!-- Mainly scripts -->
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
@@ -16,5 +33,5 @@
 
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.js"></script>
-    <script src="{{  secure_asset('/js/patient-add.js') }}"></script>
+    <script src="{{  asset('/js/patient-add.js') }}"></script>
 
--- resources/views/layouts/reset.blade.php
+++ resources/views/layouts/reset.blade.php
@@ -7,7 +7,10 @@
             <div>
 
                 <div class="row" style="justify-content: center" >
+                    <!--
                      <img src="{{  secure_asset('images/alescologo.jpg') }}" style="margin-bottom: 10px">
+                    -->
+                     <img src="{{  asset('images/alescologo.jpg') }}" style="margin-bottom: 10px">
                  </div>
 
             </div>
